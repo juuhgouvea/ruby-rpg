8ec6da5358bd1d313bfb3b958b3657c5746eeb10
juuhgouvea <juliasantos752@yahoo.com.br>
2021-03-21 23:54:53 -0300

    add calculate_attributes method to character

--- src/entities/character.rb
+++ src/entities/character.rb
@@ -8,4 +8,19 @@ class Character
     @race = race
     @klasses = klasses
   end
+
+  def calculate_attributes
+    attributes = @klasses.map do |klass| klass.modifiers end
+    attributes << @race.attributes
+
+    total = attributes.reduce({}) do |total, value|
+      value.each do |key, attribute_value|
+        total[key] = 0 unless total.include?(key)
+        total[key] += attribute_value
+      end
+      total
+    end
+    
+    total
+  end
 end
